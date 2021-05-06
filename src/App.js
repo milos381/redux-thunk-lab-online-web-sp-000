d2097c30a92b51916467b4bedb80cda177fbeee5
milos381 <milos_popovic@outlook.com>
2021-05-05 20:25:13 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux'
+import { fetchCats } from './actions/catActions';
+import CatList from './CatList.js';
 class App extends Component {   
-  
+  componentDidMount() {
+    //second, an initial state
+    console.log(this.props)
+    //fifth is calling the fetch and getting back the results
+    this.props.fetchCats()
+  }
   render() {
+    //first this, an emptu array as a result of an initial, first render
+    //fourth, again empty as a result of 
+    //sending our first dispatch({type: 'LOADING_CATS'}), 
+    //which happens before the fetch request is executed.
+    console.log(this.props.catPics)
+    
     return (
-      <div>
+      <div className="App">
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        {<CatList catPics={this.props.catPics} loading={this.props.loading}/>}
       </div>
     );
   }
+} 
+const mapStateToProps = (state) => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+}
+const dispatchToProps = (dispatch) => {
+  return {
+    fetchCats: () => dispatch(fetchCats())
+  }
 }
 
-export default App
-
+export default connect (mapStateToProps, dispatchToProps)(App)
+/*
+.then(() => { console.log("World!"); })
+  .then(() => {
+    sleep(2000)
+      .then(() => { console.log("Goodbye!"); })
+    });    */
--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,24 @@
-// write your CatList component here
+import React from 'react';
+//
+/*        if (this.props.loading === true) {
+            'LOADING CATS'
+        } else {
+            setTimeout(() => {
+                this.props.catPics.map(img => <div key={img.id}><img src={img.url} alt='cat'></img></div>)
+            }, 1000);
+        } */
+class CatList extends React.Component {
+    
+  render() {
+    return (
+      <div>
+        {
+            (this.props.loading === true) ? 'LOADING CATS...' : this.props.catPics.map(img => <div key={img.id}><img src={img.url} alt='cat'></img></div>)
+        }
+      </div>
+    );
+  }
+} 
+export default CatList
+
+
